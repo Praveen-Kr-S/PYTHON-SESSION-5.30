311a5ec39147750e660204c6fa7696c5b80406d4
PraveenKumar S <praveenkrsslm69@gmail.com>
2026-03-24 18:58:55 +0530

    Add files via upload

---
+++ file_handing.py
@@ -0,0 +1,41 @@
+#File Handing
+#create the txt file
+# c = open(r"C:\Users\Livewire\Desktop\prakash.txt","x")
+# c.close()
+# print("File create")
+
+
+#File Reading
+# r = open(r"C:\Users\Livewire\Desktop\prakash.txt","r")
+# data = r.read()
+# print(data)
+# r.close()
+
+
+#File write
+# w = open(r"C:\Users\Livewire\Desktop\vijay.txt","w")
+# w.write("Hello Praveen.")
+# w = open(r"C:\Users\Livewire\Desktop\vijay.txt","r")
+# data = w.read()
+# print(data)
+# w.close()
+
+#File append
+# w = open(r"C:\Users\Livewire\Desktop\vijay123.txt","a")
+# w.write("\nI am from chennai")
+# w = open(r"C:\Users\Livewire\Desktop\vijay123.txt","r")
+# data = w.read()
+# print(data)
+# w.close()
+
+# remove
+# import os
+# os.remove(r"C:\Users\Livewire\Desktop\kasi.txt")
+# print("File removed")
+
+
+
+
+
+
+
