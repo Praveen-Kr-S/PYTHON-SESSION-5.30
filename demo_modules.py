2bec81b6d42ed4e52b812a3adac06ddda121a26a
PraveenKumar S <praveenkrsslm69@gmail.com>
2026-03-31 11:02:31 +0530

    Add files via upload

---
+++ demo_modules.py
@@ -0,0 +1,34 @@
+
+#help("modules")
+#module ->collection variables,function and classes
+#1.math
+
+# import math
+# #module_name.collection variables,function
+# print(math.pi)
+# print(math.pow(5,2))
+# print(math.factorial(5))
+# print(math.ceil(5.1))
+# print(math.floor(5.9))
+# print(math.log(56))
+# print(math.sqrt(76))
+
+
+#random
+import random as rd
+#print(rd.random())
+# print(rd.randint(10,20))
+# print(rd.randrange(10,30,5))
+# l = [20,30,59,23,56,89]
+# print(rd.choice(l))
+# print(rd.choices(l,k=2))
+
+#calendar
+import calendar as c
+
+# print(c.calendar(2026))
+# print(c.month(2026,3))
+# print(c.isleap(2028))
+# print(c.leapdays(2000,2030))
+
+
