4fdc78716666ad00debbc9353c6e0b5ad9a3ad72
PraveenKumar S <praveenkrsslm69@gmail.com>
2026-04-02 19:00:23 +0530

    Add files via upload

---
+++ learn_modules.py
@@ -0,0 +1,67 @@
+#modules
+"""
+modules =  collection of functions,variables,class -> math,random,calendar,webbrowser,pywhatkit,time,datetime
+package = collction of modules ->numpy,pandas,scipy,matplotlib
+"""
+import datetime
+import datetime as dt
+# ct = datetime.datetime.now()
+# print(ct)
+# print(ct.year)
+# print(ct.month)
+# print(ct.day)
+# print(ct.hour)
+# print(ct.minute)
+# tt = dt.datetime(2027,1,1,0,0,0,000000)
+# print(tt)
+# print(tt.year)
+# print(tt.month)
+#
+# #find nw year date
+# print(tt-ct)
+
+
+
+import pywhatkit as pk
+
+# pk.search("Vijay")
+# pk.playonyt("TVK")
+# pk.sendwhatmsg_instantly("+91 9442546863","Hello Kasinathan")
+#pk.show_history()
+
+
+import webbrowser as wb
+# wb.open_new("https://ugc.vinayakamission.com/university")
+# wb.open_new_tab("https://livewiresalem.com/gallery.php")
+
+
+import time as t
+
+# print("Bye Java")
+# t.sleep(5)
+# print("Hello Python")
+# print(t.ctime())
+
+# print(t.strftime("%d-%m-%y %H:%M:%S"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
